f0cd99e43b35b3bfa7aa175d485c63e711150741
Bruce McTigue <bmctigue@tiguer.com>
2019-01-07 15:56:36 -0700

    Initial Commit

---
+++ Tally/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tally
+//
+//  Created by Bruce McTigue on 1/7/19.
+//  Copyright © 2019 tiguer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
