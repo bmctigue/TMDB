74084fd14b2f9f7965185538c26c98a6d232c90f
Bruce McTigue <bmctigue@tiguer.com>
2019-02-11 06:18:01 -0700

    Moved the storyboard factory to Tiguer.

--- Pods/Tiguer/Tiguer/StoryboardFactory.swift
+++ Pods/Tiguer/Tiguer/StoryboardFactory.swift
@@ -1,6 +1,6 @@
 //
 //  StoryboardFactory.swift
-//  TMDB
+//  Tiguer
 //
 //  Created by Bruce McTigue on 11/30/16.
 //  Copyright © 2016 tiguer. All rights reserved.
@@ -8,12 +8,11 @@
 
 import UIKit
 
-protocol StoryboardFactoryProtocol {
-    func create(name: String) -> UIStoryboard
-}
-
-struct StoryboardFactory: StoryboardFactoryProtocol {
-    func create(name: String) -> UIStoryboard {
+public struct StoryboardFactory: StoryboardFactoryProtocol {
+    
+    public init() {}
+    
+    public func create(name: String) -> UIStoryboard {
         let storyboard = UIStoryboard(name: name, bundle: nil)
         return storyboard
     }
