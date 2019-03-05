0e04cfd9e2888cb9dd9af32ae1ff20b15951283f
Bruce McTigue <bmctigue@tiguer.com>
2019-03-05 08:26:40 -0700

    Fixed retain cycle

--- TMDB/Scenes/Movies/ApplicationLayer/MoviesPresenter.swift
+++ TMDB/Scenes/Movies/ApplicationLayer/MoviesPresenter.swift
@@ -29,32 +29,34 @@ extension Movies {
         }
         
         override func updatedViewModels(completionHandler: @escaping ([ViewModel]) -> Void) {
-            background.dispatch {
-                var resultModels = self.viewModels
-                
-                if self.filterState == .favorite {
-                    resultModels = resultModels.filter {
-                        let model = $0 as! MovieViewModel
-                        return self.favoritesManager.getSelections().contains(model.selectionId)
+            background.dispatch { [weak self] in
+                if let self = self {
+                    var resultModels = self.viewModels
+                    
+                    if self.filterState == .favorite {
+                        resultModels = resultModels.filter {
+                            let model = $0 as! MovieViewModel
+                            return self.favoritesManager.getSelections().contains(model.selectionId)
+                        }
                     }
-                }
-                
-                if self.sortState == .ascending {
-                    resultModels = resultModels.sorted (by: {
-                        let lhs = $0 as! MovieViewModel
-                        let rhs = $1 as! MovieViewModel
-                        return lhs.popularity < rhs.popularity
-                    })
-                } else if self.sortState == .descending {
-                    resultModels = resultModels.sorted (by: {
-                        let lhs = $0 as! MovieViewModel
-                        let rhs = $1 as! MovieViewModel
-                        return lhs.popularity > rhs.popularity
-                    })
-                }
-                
-                self.main.dispatch {
-                    completionHandler(resultModels)
+                    
+                    if self.sortState == .ascending {
+                        resultModels = resultModels.sorted (by: {
+                            let lhs = $0 as! MovieViewModel
+                            let rhs = $1 as! MovieViewModel
+                            return lhs.popularity < rhs.popularity
+                        })
+                    } else if self.sortState == .descending {
+                        resultModels = resultModels.sorted (by: {
+                            let lhs = $0 as! MovieViewModel
+                            let rhs = $1 as! MovieViewModel
+                            return lhs.popularity > rhs.popularity
+                        })
+                    }
+                    
+                    self.main.dispatch {
+                        completionHandler(resultModels)
+                    }
                 }
             }
         }
